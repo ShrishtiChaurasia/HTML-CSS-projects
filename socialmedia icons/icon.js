a831c391d72ff2a1231dc02acc9804f7f1247f4f
shrishti chaurasia <131389624+ShrishtiChaurasia@users.noreply.github.com>
2023-10-20 18:30:36 +0100

    Socialmedia-icons
    Hoverable socialmedia icons.

---
+++ socialmedia icons/icon.js
@@ -0,0 +1,41 @@
+tippy('#face', {
+    content: "Facebook",
+    placement: 'top',
+    arrow: true,
+    animation: 'scale-extreme',
+    theme: 'translucent',
+    interactive: true,
+    delay: 500, // ms
+    followCursor: true,
+  });
+  tippy("#tweet", {
+    content: "Twitter",
+    placement: 'top',
+    arrow: true,
+    animation: 'scale-extreme',
+    theme: 'translucent',
+    interactive: true,
+    delay: 500, // ms
+    followCursor: true,
+  });
+  tippy('#insta', {
+    content: "Instagram",
+    placement: 'top',
+    arrow: true,
+    animation: 'scale-extreme',
+    theme: 'translucent',
+    interactive: true,
+    delay: 500, // ms
+    followCursor: true,
+  });
+  tippy('#link', {
+    content: "Linkdin",
+    placement: 'top',
+    arrow: true,
+    animation: 'scale-extreme',
+    theme: 'translucent',
+    interactive: true,
+    delay: 500, // ms
+    followCursor: true,
+  });
+  
